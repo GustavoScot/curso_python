be7a3ccf2890fcaecaebb7b4906f62287c483f70
Gustavo M. Scot <123573482+GustavoScot@users.noreply.github.com>
2025-02-26 10:53:48 -0300

    Update string.py

--- Fundamentos/string.py
+++ Fundamentos/string.py
@@ -26,10 +26,20 @@ print(tamanho2)
 
 texto4 = 'ola'
 texto5 = ' TUDO certo?'
-texto6 = 'muito bom'
-modificado = texto4.upper()
-modificado2 = texto5.lower()
-modificado3 = texto6.title()
+texto6 = ' muito bom '
+modificado = texto4.upper() # deixa tudo maiusculo 
+modificado2 = texto5.lower() # deixa tudo minusculo
+modificado3 = texto6.title() # deixa a primeira letra das palavras em maiusculo
+modificado4 = texto6.strip() # remove os espacos em branco em cada lado da string
+modificado5 = texto6.rstrip() # remove os espacos em branco do lado direito da string
+modificado6 = texto6.lstrip() # remove os espacos em branco do lado esquerdo da string
+modificado7 = texto6.replace('bom', 'legal') # substitui a string
+
 print(modificado)
 print(modificado2)
 print(modificado3)
+print(modificado4)
+print(modificado5)
+print(modificado6)
+print(modificado7)
+
